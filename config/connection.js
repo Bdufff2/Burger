826bf1368609ce45b3c20bb9fd195238e46c5012
duff.brian21@gmail.com <duff.brian21@gmail.com>
2018-01-16 16:59:05 -0800

    established connection w/ mySQL

---
+++ config/connection.js
@@ -0,0 +1,28 @@
+// Setup connection to mySQL
+var mySQL = require('mysql');
+var connection;
+
+if(process.env.JAWSDB_URL) {
+    connection = mySQL.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mySQL.createConnection({
+        port: 3306,
+        host: "localhost",
+        username: "root",
+        password: "",
+        database: "burgers_DB"
+
+    });
+}
+// Establish connection
+connection.connect(function(err){
+    if(err){
+        console.error("error connecting: " + err.stack);
+        return;
+    } else {
+        console.log("connected as id: " + connection.threadId);
+    }
+});
+
+// Export the connection for orm
+module.exports = connection;
---
+++ server.js
@@ -0,0 +1,24 @@
+// Setup dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+
+var routes = require('./controllers/burger_controller.js')
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Setup handlebars
+app.engine('handlebars', exphbs({ defaultlayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+app.use(routes);
+
+app.listen(PORT, function(){
+    console.log('App is listening on port: ' + PORT);
+});
